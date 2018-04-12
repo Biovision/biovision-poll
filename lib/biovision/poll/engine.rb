064dc7cda04c3e9fb243043b75f116e8aa6be040
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-04-12 15:08:45 +0300

    Updated bundle

--- lib/biovision/poll/engine.rb
+++ lib/biovision/poll/engine.rb
@@ -1,14 +1,18 @@
 module Biovision
   module Poll
-    class Engine < ::Rails::Engine
-      config.assets.precompile << %w(admin.scss)
-      config.assets.precompile << %w(biovision/base/icons/*)
-      config.assets.precompile << %w(biovision/base/placeholders/*)
-    end
-
     require 'biovision/base'
     require 'carrierwave'
     require 'carrierwave-bombshelter'
     require 'mini_magick'
+
+    class Engine < ::Rails::Engine
+      config.assets.precompile << %w(admin.scss)
+      config.assets.precompile << %w(biovision/base/**/*)
+
+      config.generators do |g|
+        g.test_framework :rspec
+        g.fixture_replacement :factory_bot, :dir => 'spec/factories'
+      end
+    end
   end
 end
